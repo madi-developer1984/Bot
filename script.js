01347b23726deb0db920d047830d1fb0ae2aef0c
madi-developer1984 <madi84qi@gmail.com>
2023-12-23 18:46:11 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function guessingGame() {
+    const secretNumber = Math.floor(Math.random() * 100) + 1;
+
+    function askUser() {
+        const userGuess = prompt("Угадай число от 1 до 100:");
+
+        if (userGuess === null) {
+            alert("Игра окончена.");
+            return;
+        }
+
+        if (isNaN(userGuess)) {
+            alert("Введи число!");
+            askUser(); 
+            return;
+        }
+
+        const guess = parseInt(userGuess);
+
+        if (guess === secretNumber) {
+            alert("Поздравляю, Вы угадали!!!");
+        } else if (guess > secretNumber) {
+            alert("Загаданное число меньше");
+            askUser();
+        } else {
+            alert("Загаданное число больше");
+            askUser();
+        }
+    }
+
+    askUser();
+}
+
+guessingGame();
