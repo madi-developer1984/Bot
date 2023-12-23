8b4ad67e15996ff002ae82eadaf58f98d01683ed
madi-developer1984 <madi84qi@gmail.com>
2023-12-23 20:38:44 +0600

    Добавлены функции и выполнены задачи

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function guessingGame() {
-    const secretNumber = Math.floor(Math.random() * 100) + 1;
+const game = {
+    secretNumber: 0,
+    userAttempts: 0,
+    maxAttempts: 3,
 
-    function askUser() {
-        const userGuess = prompt("Угадай число от 1 до 100:");
+    start() {
+        this.secretNumber = Math.floor(Math.random() * 100) + 1;
+        this.askUser();
+        this.logger();
+    },
+
+    askUser() {
+        this.userAttempts++;
+        const userGuess = prompt(`Угадай число от 1 до 100 (попытка ${this.userAttempts} из ${this.maxAttempts}):`);
 
         if (userGuess === null) {
             alert("Игра окончена.");
@@ -11,24 +20,37 @@ function guessingGame() {
 
         if (isNaN(userGuess)) {
             alert("Введи число!");
-            askUser(); 
+            this.askUser();
             return;
         }
 
         const guess = parseInt(userGuess);
 
-        if (guess === secretNumber) {
+        if (guess === this.secretNumber) {
             alert("Поздравляю, Вы угадали!!!");
-        } else if (guess > secretNumber) {
+        } else if (guess > this.secretNumber) {
             alert("Загаданное число меньше");
-            askUser();
+            if (this.userAttempts < this.maxAttempts) {
+                this.askUser();
+            }
         } else {
             alert("Загаданное число больше");
-            askUser();
+            if (this.userAttempts < this.maxAttempts) {
+                this.askUser();
+            }
+        }
+    },
+
+    logger() {
+        console.log("Состояние объекта game:");
+
+        for (const property in this) {
+            if (typeof this[property] !== "function") {
+                console.log(`${property}: ${this[property]}`);
+            }
         }
     }
+};
 
-    askUser();
-}
-
-guessingGame();
+// Запуск игры
+game.start();
